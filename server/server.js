80f01fb289dabcd7c383a8f3bb3956036ae81bff
Nick Hester <nick.d.hester@accruent.com>
2020-02-19 17:13:23 -0600

    removing the build routing

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, '../build')));
+// app.use(express.static(path.join(__dirname, '../build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 const port = process.env.PORT || 8080;

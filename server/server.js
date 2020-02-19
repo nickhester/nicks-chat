728d2e0c707b018ea51067449957cb66131bc066
Nick Hester <nick.d.hester@accruent.com>
2020-02-18 20:29:22 -0600

    more configuration

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.get('/conversation', function (req, res) {
